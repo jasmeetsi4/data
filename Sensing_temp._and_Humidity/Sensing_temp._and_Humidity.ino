d6f3679918d9ebf848308e1593898f17da807036
jasmeetsi4 <38372611+jasmeetsi4@users.noreply.github.com>
2019-08-28 15:13:41 +1000

    Add files via upload

---
+++ Sensing_temp._and_Humidity/Sensing_temp._and_Humidity.ino
@@ -0,0 +1,32 @@
+//Library Files
+#include <DHT.h>
+#include <DHT_U.h> 
+#include <Adafruit_Sensor.h>
+
+
+
+#define DHTPIN 2     
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE); 
+
+float hum; 
+float temp;
+void setup()
+{
+    Serial.begin(9600);
+    dht.begin();
+}
+
+void loop()
+{
+  
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(2000);
+}
---
+++ soil/soil.ino
@@ -0,0 +1,22 @@
+int LED=13;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val;
+  val=analogRead(0);
+  Serial.println(val);
+  if(val<300)
+  {
+    digitalWrite(LED,HIGH);
+  }
+  else
+  {
+    digitalWrite(LED,LOW);
+  }
+  delay(1000);
+}
